b580d9456c7cbb4a0613cd6f488c1229d47e41f0
José Ángel Martín <joseangelmartinb@gmail.com>
2017-04-08 21:10:30 +0200

    Add red_hipercubo. Some minor changes in red_toroide. The implementation of the code has been finished.

--- src/red_hipecubo.c
+++ src/red_hipecubo.c
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* red_toroide.c                                                              *
+* red_hipecubo.c                                                             *
 * Autor: José Ángel Martín Baos.                                             *
 * Diseño de Infraestructura de Red - Práctica 1                              *
 * 2017                                                                       *
@@ -8,7 +8,8 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
-#define L 4
+#include <math.h>
+#define D 4 // Number of dimensions
 #define DATOS_FILE "datos.dat"
 
 void read_and_distribute_token(int* token, int rank, int numtasks){
@@ -47,52 +48,32 @@ void obtain_token(int* token, int rank){
   }
 }
 
-void obtain_neighbors(int* north, int* south, int* east, int* west, int rank,
-  int numtasks){
-  //North neighbor
-  *north = (rank + L) % numtasks;
-
-  //South neighbor
-  *south = (rank - L) % numtasks;
-  if(*south < 0){
-    *south += numtasks;
+int obtain_neighbor(int rank, int dimension){
+  int neighbor, node;
+  for(node = 0; node < (int)pow(2,D); node++){
+    if((rank ^ node) == (int)pow(2,dimension - 1)){
+      neighbor = node;
+      break;
+    }
   }
-
-  //East neighbor
-  *east = rank + 1;
-  if(*east % L == 0){
-    *east -= L;
-  }
-
-  //West neighbor
-  *west = (rank - 1) % numtasks;
-  if(*west % L == L-1){
-    *west += L;
-  }else if(*west == -1){
-    *west = L-1;
-  }
+  return neighbor;
 }
 
 int main (int argc, char *argv[]){
   int numtasks,       // Number of MPI tasks
       rank,           // Task number of the current process
-      north,          // Rank of the north neighbor
-      south,          // Rank of the south neighbor
-      east,           // Rank of the east neighbor
-      west,           // Rank of the west neighbor
       token;          // The number from datos.dat for this process
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-  if (numtasks != L*L && rank == 0){
-    printf("Error: %d task are needed.", L*L);
+  if (numtasks != (int)pow(2,D) && rank == 0){
+    printf("Error: %d task are needed.\n", (int)pow(2,D));
+    fflush(stdout);
     MPI_Abort(MPI_COMM_WORLD, 1);
     exit(1);
   }
-
-  obtain_neighbors(&north, &south, &east, &west, rank, numtasks);
 
   if(rank == 0){
     read_and_distribute_token(&token, rank, numtasks);
@@ -103,46 +84,30 @@ int main (int argc, char *argv[]){
   // Algorithm
   int i, in_token, rc;
   MPI_Status status;
-  for (i = 0; i < L-1; i++){
-    rc = MPI_Send(&token, 1, MPI_INT, south, i, MPI_COMM_WORLD);
+  for (i = 1; i <= D; i++){
+    rc = MPI_Send(&token, 1, MPI_INT, obtain_neighbor(rank, i), i,
+                  MPI_COMM_WORLD);
     if (rc != MPI_SUCCESS) {
        printf("Send error in task %d\n", rank);
        MPI_Abort(MPI_COMM_WORLD, rc);
        exit(1);
     }
-    rc = MPI_Recv(&in_token, 1, MPI_INT, north, MPI_ANY_TAG, MPI_COMM_WORLD,
-                  &status);
-    if (rc != MPI_SUCCESS) {
-       printf("Receive error in task %d\n", rank);
-       MPI_Abort(MPI_COMM_WORLD, rc);
-       exit(1);
-    }
-    if(in_token < token){
-      token = in_token;
-    }
-  }
 
-  for (i = 0; i < L-1; i++){
-    rc = MPI_Send(&token, 1, MPI_INT, east, i, MPI_COMM_WORLD);
-    if (rc != MPI_SUCCESS) {
-       printf("Send error in task %d\n", rank);
-       MPI_Abort(MPI_COMM_WORLD, rc);
-       exit(1);
-    }
-    rc = MPI_Recv(&in_token, 1, MPI_INT, west, MPI_ANY_TAG, MPI_COMM_WORLD,
-                  &status);
+    rc = MPI_Recv(&in_token, 1, MPI_INT, obtain_neighbor(rank, i), MPI_ANY_TAG,
+                  MPI_COMM_WORLD, &status);
     if (rc != MPI_SUCCESS) {
        printf("Receive error in task %d\n", rank);
        MPI_Abort(MPI_COMM_WORLD, rc);
        exit(1);
     }
-    if(in_token < token){
+
+    if(in_token > token){
       token = in_token;
     }
   }
 
   if(rank == 0){
-    printf("El menor elemento de toda la red es %d.\n", token);
+    printf("El mayor elemento de toda la red es %d.\n", token);
   }
 
   MPI_Finalize();
